ddbc6034d9d6173bfa6cdd2431ce3721d9c4ced7
Kirkland Gee <kirkladgee12@gmail.com>
2022-10-10 08:50:58 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [siteURL, setSiteURL] = useState("https://gr0.com/");
+  const [startDate, setStartDate] = useState("09-01-2022");
+  const [endDate, setEndDate] = useState("09-30-2022")
+  const [dimensions, setDimensions] = useState(["PAGE", "QUERY"]);
+  const [dimensionFilterGroups, setDimensionFilterGroups] = useState([{"filters": [{"dimension": "PAGE", "operator": "CONTAINS", "expression": "/case-studies"}]}]);
+  // const [filters, setFilters] = useState([]);
+  // const [filterOperator, setFilterOperator] = useState([]);
+  const [rowLimit, setRowLimit] = useState(1000);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={siteURL}
+          placeholder="Site URL"
+          onChange={(e) => setSiteURL(e.target.value)}
+        />
+        <input
+          type="date"
+          value={startDate}
+          placeholder="09-01-2022"
+          onChange={(e) => setStartDate(e.target.value)}
+        />
+        <input
+          type="date"
+          value={endDate}
+          placeholder="09-30-2022"
+          onChange={(e) => setEndDate(e.target.value)}
+        />
+        <input
+          type="text"
+          value={mobileNumber}
+          placeholder="Mobile Number"
+          onChange={(e) => setMobileNumber(e.target.value)}
+        />
+
+        <button type="submit">Create</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
     </div>
   );
 }
